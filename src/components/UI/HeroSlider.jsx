7f45e9dbc77a49096a3cb33fe057405e9e589c5e
ulukbekski <88556249+ulukbekski@users.noreply.github.com>
2023-02-01 18:27:57 +0600

    Незначительные изменения

--- src/components/UI/HeroSlider.jsx
+++ src/components/UI/HeroSlider.jsx
@@ -9,8 +9,8 @@ import "../../styles/hero-slider.css";
 const HeroSlider = () => {
   const settings = {
     fade: true,
-    speed: 2000,
-    autoplaySpeed: 3000,
+    speed: 3000,
+    autoplaySpeed: 5000,
     infinite: true,
     autoplay: true,
     slidesToShow: 1,
